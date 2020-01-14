e267178a0593487de5b5e63f09e0b7cd67ea7802
Husak Yurii <gusak.1881@gmail.com>
2020-01-14 13:03:27 +0200

    Sub model has been created for a cell on the grid

---
+++ src/components/field/sub-models/CellModel.js
@@ -0,0 +1,15 @@
+import { Model } from "../../../../libs/component/Model";
+
+class CellModel extends Model {
+    constructor({ x, y, row, col, type, width, height }) {
+        super();
+
+        this.x = x;
+        this.y = y;
+        this.row = row;
+        this.col = col;
+        this.type = type;
+        this.width = width;
+        this.height = height;
+    }
+}

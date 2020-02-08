2593faac870df641f48692a734fd719dbac8ffd8
Husak Yurii <gusak.1881@gmail.com>
2020-02-08 16:54:33 +0100

    Engine was updated with new algorithms for game mechanic

--- src/components/field/engine/Engine.js
+++ src/components/field/engine/Engine.js
@@ -9,7 +9,8 @@ export class Engine {
     }
 
     generateGrid({ rows, columns }) {
-        const grid = Array.from({ length: rows }).map(_ => Array.from({ length: columns }).fill(0));
+        const grid = Array.from({ length: rows })
+            .map((_) => Array.from({ length: columns }).fill(0));
 
         return grid.map((row, rowIndx) => {
             return row.map((type, colIndx) => ({ type, row: rowIndx, col: colIndx }));
@@ -26,7 +27,7 @@ export class Engine {
             };
         };
 
-        return emptyGrid.map(gridRow => {
+        return emptyGrid.map((gridRow) => {
             return gridRow.map(({ type, row, col }) => {
                 const pos = calcPosition(filedSizes, cellSizes, col, row);
                 return { row, col, type, ...cellSizes, ...pos };
@@ -35,24 +36,70 @@ export class Engine {
     }
 
     generateRandomCells(gridData) {
-        const emptyCells = gridData.flat().filter(({ type }) => type === 0);
-        let indx = Engine.randomInt(0, emptyCells.length);
-        let counter = 0;
+        const emptyCells = gridData.flat()
+            .filter(cell => cell.type === 0);
 
-        let cellData;
+        const type = Engine.randomInt(0, 10) < 5 ? 2 : 4;
+        let indx = Engine.randomInt(0, emptyCells.length);
 
         while (true) {
-            if (counter >= emptyCells.length) return;
-            else counter += 1;
-
             if (emptyCells[indx].type === 0) break;
             else indx = Engine.randomInt(0, emptyCells.length);
         }
 
-        return [Object.assign({}, emptyCells[indx], { type: 2 })];
+        return [{ create: [{ ...emptyCells[indx], type }] }]
+    }
+
+    slideGrid(gridData) {
+        return gridData.map((gridRow) => this.slideRow(gridRow))
+            .flat();
+    }
+
+    // TODO think of optimizing this mess
+    slideRow(arr) {
+        const results = [];
+        if (arr.length === 0) return results;
+
+        const first = arr.shift();
+        const next = arr.find(val => val.type !== 0);
+
+        if (!next) {
+            return this.slideRow(arr);
+        } else if (first.type === 0) {
+            results.push({
+                from: { row: next.row, col: next.col },
+                to: { row: first.row, col: first.col },
+                create: [{ ...first, type: next.type }],
+                remove: [{ ...next }]
+            });
+            first.type = next.type;
+            next.type = 0;
+            arr.unshift(first);
+        } else if (first.type !== next.type) {
+            if (first.col + 1 !== next.col) {
+                results.push({
+                    from: { row: next.row, col: next.col },
+                    to: { row: first.row, col: first.col + 1 },
+                    create: [{ ...arr[0], type: next.type }],
+                    remove: [{ ...next }]
+                });
+                arr[0].type = next.type;
+                next.type = 0;
+            }
+        } else if (first.type === next.type) {
+            results.push({
+                from: { row: next.row, col: next.col },
+                to: { row: first.row, col: first.col },
+                create: [{ ...first, type: first.type + next.type }],
+                remove: [{ ...first }, { ...next }]
+            });
+            next.type = 0;
+        }
+
+        return [...results, ...this.slideRow(arr)];
     }
 }
 
 Engine.randomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-}
+};

3bbc7667a768858bda88041f5d60375cf204e3c1
Husak Yurii <gusak.1881@gmail.com>
2020-02-10 00:39:12 +0100

    Tools component was updated

--- src/components/shared/Tools.js
+++ src/components/shared/Tools.js
@@ -13,4 +13,17 @@ const randomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 };
 
-export { invokeAfter, randomInt }
+
+const turnColumnsToRows = function (grid) {
+    const turned = [];
+    for (let col = 0; col < grid[0].length; col += 1) {
+        const newRow = [];
+        for (let row = 0; row < grid.length; row += 1) {
+            newRow.push(grid[row][col]);
+        }
+        turned.push(newRow);
+    }
+    return turned;
+};
+
+export { invokeAfter, randomInt, turnColumnsToRows };
